29f8e9c60830c193d7bb3ad3388289fb86cfdff4
therk074 <phatarisa.m@fwd.com>
2026-02-21 09:34:38 +0700

    refactor: :recycle: Container children

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'My Title',
-    home: Scaffold(
-      appBar:AppBar(
-        title: const Text("My App"),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
+  runApp(
+    MaterialApp(
+      title: 'My Title',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("My App"),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+        ),
+        body: const Home(),
       ),
-      body: const Home(),
     ),
-  ));
+  );
 }
 
 class Home extends StatelessWidget {
-  const Home ({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueGrey,
-      // margin: EdgeInsets.all(10),
-      // margin: const EdgeInsets.fromLTRB(10, 1, 9, 30),
-      margin: EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          Text('Title'),
+          SizedBox(height: 8),
+          Text('Subtitle'),
+          ElevatedButton(onPressed: null, child: Text('Button')),
+        ],
+      ),
     );
   }
 }
