853b1300cc0059fb0deb0029bd2a12b10c25c885
therk074 <phatarisa.m@fwd.com>
2026-02-21 09:15:06 +0700

    feat: :sparkles: StatelessWidget Text

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,16 @@ void main() {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: const Text("Body Flutter"),
+      body: const Home(),
     ),
   ));
 }
 
+class Home extends StatelessWidget {
+  const Home ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello Flutter Content");
+  }
+}
